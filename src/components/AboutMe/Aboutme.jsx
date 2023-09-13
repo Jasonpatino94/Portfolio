21231dc9f94f38c41f35592a5b662ccb50a0c669
Jason <Jason.patino94@gmail.com>
2023-09-13 14:58:50 -0400

    added more skills

--- src/components/AboutMe/Aboutme.jsx
+++ src/components/AboutMe/Aboutme.jsx
@@ -21,32 +21,29 @@ const Aboutme = () => {
 		});
 	}, []);
 	return (
-		<div className="aboutme" id="aboutme">
-			<div className="picture">
-				<div className="me">
-					<img src="Pictures/me2.jpg" alt="Jason" />
-				</div>
-			</div>
-			<div className="content">
-				<div className="text">
-					{/* <h3>Hello My name is</h3>
-					<h1>Jason Patino</h1> */}
-					<h3>
-						<span className="titlespin"></span> <br />
-						<span className="title2" ref={title2Ref}></span>
-					</h3>
+    <div className="aboutme" id="aboutme">
+      <div className="picture">
+        <div className="me">
+          <img src="Pictures/me2.jpg" alt="Jason" />
+        </div>
+      </div>
+      <div className="content">
+        <div className="text">
+          <h3>
+            <span className="titlespin"></span> <br />
+            <span className="title2" ref={title2Ref}></span>
+          </h3>
 
-					<h3 className="summary">
-						Hello, my name is <span className="name">Jason Patino</span> and I
-						am a recent graduate from Flatiron School. In my free time I love to
-						watch videos and read more about coding, and if I'm not doing that
-						then I am most likely at the firehouse volunteering for the
-						comumunity as a lieutenant.{" "}
-					</h3>
-				</div>
-			</div>
-		</div>
-	);
+          <h3 className="summary">
+            Hello, my name is <span className="name">Jason Patino</span>. I am
+            an aspiring developer. In my free time I love to watch videos and
+            read more about coding and different softwares. I also enjoy working
+            on car, traveling, and photography/videography. I have also been a volunteer firefighter for 8 years.
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Aboutme;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,44 +6,44 @@ import {useEffect, useState} from "react";
 
 const Projects = () => {
 	const sidebarlist = [
-		{
-			id: 1,
-			name: "Dog Breed",
-			git: "https://github.com/Jasonpatino94/My-Cli-Project",
-			video: "https://vimeo.com/manage/videos/444956672",
-		},
-		{
-			id: 2,
-			name: "Vehicle Inventory",
-			git: "https://github.com/Jasonpatino94/Sinatra_project_inventories",
-			video: "https://vimeo.com/manage/videos/464845300",
-		},
-		{
-			id: 3,
-			name: "Vehicle Search App",
-			git: "https://github.com/Jasonpatino94/Vehicle-search",
-			video: "https://vimeo.com/488309006",
-		},
-		{
-			id: 4,
-			name: "Hangman",
-			git: "https://github.com/Jasonpatino94/Hangman",
-			video: "https://vimeo.com/509313071",
-		},
-		{
-			id: 5,
-			name: "Workout App",
-			git: "https://github.com/Jasonpatino94/My_workout_app",
-			video: "https://vimeo.com/575056140",
-		},
-	];
+    {
+      id: 1,
+      name: "Fire Department",
+      git: "https://github.com/Jasonpatino94/FireDepartment",
+    //   video: "NA",
+    },
+    {
+      id: 2,
+      name: "Vehicle Inventory",
+      git: "https://github.com/Jasonpatino94/Sinatra_project_inventories",
+      video: "https://vimeo.com/manage/videos/464845300",
+    },
+    {
+      id: 3,
+      name: "Vehicle Search App",
+      git: "https://github.com/Jasonpatino94/Vehicle-search",
+      video: "https://vimeo.com/488309006",
+    },
+    {
+      id: 4,
+      name: "Hangman",
+      git: "https://github.com/Jasonpatino94/Hangman",
+      video: "https://vimeo.com/509313071",
+    },
+    {
+      id: 5,
+      name: "Workout App",
+      git: "https://github.com/Jasonpatino94/My_workout_app",
+      video: "https://vimeo.com/575056140",
+    },
+  ];
 	const [chosen, setChosen] = useState(1);
 	const [img, setImg] = useState([]);
 
 	useEffect(() => {
 		switch (chosen) {
 			case 1:
-				setImg("Pictures/dogbreed.PNG");
+				setImg("Pictures/firehouse.PNG");
 				break;
 			case 2:
 				setImg("Pictures/inventory.PNG");
